82b5a147e8ea9013848b7e326abd2fc1ca580c1d
Forgo7ten <Forgo7ten2020@gmail.com>
2023-01-29 00:20:33 +0800

    add keystore

--- template.js
+++ template.js
@@ -324,7 +324,6 @@ let futil = {
         });
     },
 
-
     gson_init_flag: false,
     /**
      * 将对象转成json
@@ -339,6 +338,67 @@ let futil = {
         const gson = Java.use('com.r0ysue.gson.Gson');
         return gson.$new().toJson(obj);
     },
+
+    /**
+     * [未测试] dump客户端证书，并保存为p12的格式，证书密码为Forgo7ten
+     */
+    hook_keystore: function hook_keystore() {
+        var password = 'Forgo7ten';
+
+        function getNowTime() {
+            function dateFormat(fmt, date) {
+                let ret;
+                const opt = { "Y+": date.getFullYear().toString(), "m+": (date.getMonth() + 1).toString(), "d+": date.getDate().toString(), "H+": date.getHours().toString(), "M+": date.getMinutes().toString(), "S+": date.getSeconds().toString() };
+                for (let k in opt) {
+                    ret = new RegExp("(" + k + ")").exec(fmt);
+                    if (ret) {
+                        fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+                    };
+                };
+                return fmt;
+            }
+            function random(min, max) {
+                return Math.floor(Math.random() * (max - min)) + min;
+            }
+            return dateFormat("YYYY_mm_dd_HH_MM_SS", new Date()) + "_" + random(1, 100);
+        }
+        Java.perform(function () {
+            function storeP12(privateKey, certificate, saveP12Path, p12Password) {
+                var X509Certificate = Java.use("java.security.cert.X509Certificate")
+                var p7X509 = Java.cast(certificate, X509Certificate);
+                var chain = Java.array("java.security.cert.X509Certificate", [p7X509])
+                var ks = Java.use("java.security.KeyStore").getInstance("PKCS12", "BC");
+                ks.load(null, null);
+                ks.setKeyEntry("client", privateKey, Java.use('java.lang.String').$new(p12Password).toCharArray(), chain);
+                try {
+                    var out = Java.use("java.io.FileOutputStream").$new(saveP12Path);
+                    ks.store(out, Java.use('java.lang.String').$new(p12Password).toCharArray())
+                } catch (exp) {
+                    console.log(exp)
+                }
+            }
+            Java.use("java.security.KeyStore$PrivateKeyEntry").getPrivateKey.implementation = function () {
+                var packageName = Java.use("android.app.ActivityThread").currentApplication().getApplicationContext().getPackageName();
+                var savePath = '/sdcard/Download/' + packageName;
+
+                var result = this.getPrivateKey();
+                var fileName = savePath + getNowTime() + '.p12'
+                storeP12(this.getPrivateKey(), this.getCertificate(), fileName, password);
+                console.log("dump ClinetCertificate=>", fileName, "pwd:" + password);
+                return result;
+            }
+            Java.use("java.security.KeyStore$PrivateKeyEntry").getCertificateChain.implementation = function () {
+                var packageName = Java.use("android.app.ActivityThread").currentApplication().getApplicationContext().getPackageName();
+                var savePath = '/sdcard/Download/' + packageName;
+                var result = this.getCertificateChain()
+                var fileName = savePath + getNowTime() + '.p12'
+                storeP12(this.getPrivateKey(), this.getCertificate(), fileName, password);
+                console.log("dump ClinetCertificate=>", fileName, "pwd:" + password);
+                return result;
+            }
+        });
+    },
+
     /**
      * 对toast进行hook
      */
