d1364b9e4aeb47aeea1a5a6bba53136911184d3e
Forgo7ten <Forgo7ten2020@gmail.com>
2023-02-03 16:49:00 +0800

    update hook_vpn

--- hook/hook_vpn.js
+++ hook/hook_vpn.js
@@ -1,10 +1,26 @@
 function hook_vpn(){
     Java.perform(function(){
         var NetworkInterface  = Java.use("java.net.NetworkInterface")
+        var NetworkInfo = Java.use("android.net.NetworkInfo")
+        var NetworkCapabilities = Java.use("android.net.NetworkCapabilities")
         NetworkInterface.getNetworkInterfaces.implementation = function(){
             console.log("hook_vpn NetworkInterface.getNetworkInterfaces()")
             let ret = this.getNetworkInterfaces()
             return null;
+        }
+        NetworkCapabilities.isConnected.implementation = function(){
+            console.log("hook_vpn NetworkCapabilities.isConnected()")
+            return false
+        }
+
+        NetworkInterface.getName.implementation = function(){
+            console.log("hook_vpn NetworkInterface.getName()")
+            return ""
+        }
+        
+        NetworkInfo.hasTransport.implementation=function(){
+            console.log("hook_vpn NetworkInfo.hasTransport()")
+            return false
         }
     })
 }
