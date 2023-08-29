b1529d098f82fa8c3e337d90be189f290217fd60
Forgo7ten <Forgo7ten2020@gmail.com>
2023-08-29 23:45:35 +0800

    fix: add log level

--- utils/common/flogger.ts
+++ utils/common/flogger.ts
@@ -1,7 +1,31 @@
 import Wrapper = Java.Wrapper;
 
 export class _Flog {
-    static debugFlag: boolean = true;
+    static LOG_LEVEL_DEBUG: number = 0;
+    static LOG_LEVEL_INFO: number = 1;
+    static LOG_LEVEL_WARN: number = 2;
+    static LOG_LEVEL_ERROR: number = 3;
+    static level: number = this.LOG_LEVEL_DEBUG;
+
+    /**
+     * 设置日志等级
+     * @param level 日志等级
+     */
+    static setLogLevel(level: number) {
+        switch (level) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+                this.level = level;
+                break;
+            default:
+                this.level = this.LOG_LEVEL_DEBUG;
+                this.e("Error level!")
+                break;
+        }
+    }
+
     static readonly TAG: string = "FridaLog"
 
     static line(msg: string): void;
@@ -20,7 +44,7 @@ export class _Flog {
     static d(tag: string, msg: string): void;
 
     static d(tag_or_msg: any, msg?: string): void {
-        if (this.debugFlag) {
+        if (this.LOG_LEVEL_DEBUG >= this.level) {
             if (msg) {
                 _Flog._log(console.log, 'DEBUG', tag_or_msg, msg);
             } else {
@@ -34,10 +58,12 @@ export class _Flog {
     static i(tag: string, msg: string): void;
 
     static i(tag_or_msg: any, msg?: string): void {
-        if (msg) {
-            _Flog._log(console.log, 'INFO', tag_or_msg, msg);
-        } else {
-            _Flog._log(console.log, 'INFO', _Flog.TAG, tag_or_msg);
+        if (this.LOG_LEVEL_INFO >= this.level) {
+            if (msg) {
+                _Flog._log(console.log, 'INFO', tag_or_msg, msg);
+            } else {
+                _Flog._log(console.log, 'INFO', _Flog.TAG, tag_or_msg);
+            }
         }
     }
 
@@ -46,10 +72,12 @@ export class _Flog {
     static w(tag: string, msg: string): void;
 
     static w(tag_or_msg: any, msg?: string): void {
-        if (msg) {
-            _Flog._log(console.warn, 'WARN', tag_or_msg, msg);
-        } else {
-            _Flog._log(console.warn, 'WARN', _Flog.TAG, tag_or_msg);
+        if (this.LOG_LEVEL_WARN >= this.level) {
+            if (msg) {
+                _Flog._log(console.warn, 'WARN', tag_or_msg, msg);
+            } else {
+                _Flog._log(console.warn, 'WARN', _Flog.TAG, tag_or_msg);
+            }
         }
     }
 
@@ -58,14 +86,16 @@ export class _Flog {
     static e(tag: string, msg: string): void;
 
     static e(tag_or_msg: any, msg?: string): void {
-        if (msg) {
-            _Flog._log(console.error, 'ERROR', tag_or_msg, msg);
-        } else {
-            _Flog._log(console.error, 'ERROR', _Flog.TAG, tag_or_msg);
+        if (this.LOG_LEVEL_ERROR >= this.level) {
+            if (msg) {
+                _Flog._log(console.error, 'ERROR', tag_or_msg, msg);
+            } else {
+                _Flog._log(console.error, 'ERROR', _Flog.TAG, tag_or_msg);
+            }
         }
     }
 
-    static _log(logfunc: (message?: any, ...optionalParams: any[]) => void, leval: string, tag: string, msg: string) {
+    static _log(logfunc: (message?: any, ...optionalParams: any[]) => void, level: string, tag: string, msg: string) {
         try {
             let threadName = "";
             if (Java.available) {
@@ -74,11 +104,11 @@ export class _Flog {
                     threadName = `[${(<Wrapper>Thread.currentThread()).getName()}]`;
                 });
             }
-            // logfunc(`[${leval}][${new Date().toLocaleString('zh-CN')}][PID:${Process.id}]${threadName}[${Process.getCurrentThreadId()}][${tag}]: ${msg}`);
-            logfunc(`[${leval}][${new Date().toLocaleString('zh-CN')}][${Process.id}]${threadName}[${tag}]: ${msg}`);
+            // logfunc(`[${level}][${new Date().toLocaleString('zh-CN')}][PID:${Process.id}]${threadName}[${Process.getCurrentThreadId()}][${tag}]: ${msg}`);
+            logfunc(`[${level}][${new Date().toLocaleString('zh-CN')}][${Process.id}]${threadName}[${tag}]: ${msg}`);
         } catch (err) {
             if (err instanceof ReferenceError) {
-                logfunc(`[${leval}][${new Date().toLocaleString('zh-CN')}][${tag}]: ${msg}`);
+                logfunc(`[${level}][${new Date().toLocaleString('zh-CN')}][${tag}]: ${msg}`);
             }
         }
 
